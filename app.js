1c88e7a53a7261eebcfbb675ef004afe0e003d4d
Jenna Navratil & Jordan Navratil <student@epicodus.com>
2015-09-23 15:25:04 -0700

    add initial files, along with initial services, partials, and controllers

---
+++ app.js
@@ -0,0 +1,20 @@
+var outdoorExperts = angular.module('outdoorExperts', ['ui.router']);
+
+outdoorExperts.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url: "",
+    templateUrl: "partials/home.html"
+  });
+
+  $stateProvider.state('questions', {
+    url: "/questions",
+    templateUrl: "partials/questions.html",
+    controller: 'QuestionsCtrl'
+  });
+
+  $stateProvider.state('questions.answers', {
+    url: "/:questionId",
+    templateUrl: "partials/questions.answers.html",
+    controller: 'AnswersCtrl'
+  });
+});
---
+++ services/QuestionsFactory.js
@@ -0,0 +1,10 @@
+outdoorExperts.factory('QuestionsFactory', function QuestionsFactory() {
+  var factory = {};
+  factory.questions =[];
+  factory.addQuestion = function() {
+    factory.questions.push({ title: factory.questionTitle, id: factory.questions.length +1, answers: [] });
+    factory.questionTitle = null;
+  };
+
+  return factory;
+})
